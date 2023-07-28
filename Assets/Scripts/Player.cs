d9c935a6103536539a0d4ff527d2a8d5622969e0
Sherry Fisher <sherryw17@gmail.com>
2023-07-27 21:05:43 -0600

    switch statement practice challenge

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int points;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetKeyDown(KeyCode.Q))
+        {
+            points = 50; 
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            points = 100;
+        }
+       else if (Input.GetKeyDown(KeyCode.E))
+        {
+            points = 0;
+        }
+
+      
+       switch(points)
+        {
+            case 50:
+                Debug.Log("Points are 50!");
+                break;
+
+            case 100:
+                Debug.Log("Points are 100!");
+                break;
+
+            default:
+                Debug.Log("You need 50 or 100 points to receive a message");
+                break;
+        }
+    }
+}
